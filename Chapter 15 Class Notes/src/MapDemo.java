5bf39e0ee092baf6f0781d08c7aae183ebeb9da5
Geoff Schmit <gcschmit@gmail.com>
2020-10-02 14:00:13 -0500

    example of Map

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,5 +11,25 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
+        /* 
+            The Map interface is a generic. The first type is the type of
+                the key; the second, the type of the value.
+         */
+        Map<String, Color> favoriteColors = new HashMap<>();
+
+        favoriteColors.put("Ram", Color.RED);
+        favoriteColors.put("Judson", Color.GREEN);
+        favoriteColors.put("Bill", Color.ORANGE);
+        favoriteColors.put("Nikhil", Color.BLUE);
+        favoriteColors.put("Allen", Color.GREEN);
+
+        // create a set of the keys in the map
+        Set<String> keySet = favoriteColors.keySet();
+
+        for(String key : keySet)
+        {
+            System.out.println(key + " (" + key.hashCode() + 
+                    "): " + favoriteColors.get(key));  
+        }
     }
 }
