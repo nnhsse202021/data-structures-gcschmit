caa77df35965a9524085e78f494a5c53c7e69d5b
Geoff Schmit <gcschmit@gmail.com>
2020-10-18 17:49:25 -0500

    implemented LinkedList class

--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,31 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        // create a linked list and add names to it
+        LinkedList staff = new LinkedList();
+        staff.addFirst("Tom");
+        staff.addFirst("Romeo");
+        staff.addFirst("Harry");
+        staff.addFirst("Diana");
+
+        ListIterator iterator = staff.listIterator();   // |DHRT
+        iterator.next();                                // D|HRT
+        iterator.next();                                // DH|RT
+
+        // add more elements after the second element
+        iterator.add("Juliet");                         // DHJ|RT
+        iterator.add("Maria");                          // DHJM|RT
+
+        // remove Romeo
+        iterator.next();                                // DHJMR|T
+        iterator.remove();                              // DHJM|T
+
+        // print all elements
+        iterator = staff.listIterator();
+        while(iterator.hasNext())
+        {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
     }
 }
